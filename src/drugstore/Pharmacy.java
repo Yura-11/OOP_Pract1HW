7d5ce15738526dd3d83e1f721c9872799e2701a3
Yura <romanov.e180@gmail.com>
2024-02-16 23:02:29 +0300

    final

--- src/drugstore/Pharmacy.java
+++ src/drugstore/Pharmacy.java
@@ -1,24 +1,3 @@
-package drugstore;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Pharmacy implements Comparable<Pharmacy> {
-    public List<Component> components = new ArrayList<>();
-
-
-    @Override
-    public int compareTo(Pharmacy that) {
-        int fullPowerThat = 0;
-        int fullPowerThis = 0;
-
-        for (Component c : components) fullPowerThis += c.getPower();
-        for (Component c : that.components) fullPowerThat += c.getPower();
-
-        return Integer.compare(fullPowerThis, fullPowerThat);
-    }
-}
-
 
 
 
